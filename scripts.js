cbe32489d8d3d6657d6eb9c0f9ae83b2f10f789e
DenoIsBetterThanNodeJS <162937938+DenoIsBetterThanNodeJS@users.noreply.github.com>
2024-08-28 17:46:52 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+alert("This website is new, and it is still underdevelopment. If you have any suggestions, post it on youtube.");
+
+function calculator() {
+    window.open("https://simplycalc.w3spaces.com");
+}
+const calc = document.getElementById("open_simply_calc");
+
+calc.onlick = function() {calculator()};
